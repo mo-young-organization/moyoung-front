41a61977073a632771fbdc2ba5f18cc32183b20a
LEE2302 <dltpdudqkqh1@gmail.com>
2023-08-11 13:11:25 +0900

    design: 영화 검색 페이지디자인  수정

--- src/components/MovieSearch/MovieTopFive.tsx
+++ src/components/MovieSearch/MovieTopFive.tsx
@@ -11,14 +11,19 @@ type ImgProps = {
 const MovieTopFive = ({ dummy }: Props) => {
   return (
     <Container>
-      {dummy.map((el, idx) => (
-        <div key={idx}>
-          <MovieBox img={el.JPG}>
-            <span className="number">{idx + 1}</span>
-          </MovieBox>
-          <MovieTitle>{el.MOVIE}</MovieTitle>
-        </div>
-      ))}
+      <Title>
+        <div className="top5">{`TOP${dummy.length}`}</div>
+      </Title>
+      <Content>
+        {dummy.map((el, idx) => (
+          <div key={idx}>
+            <MovieBox img={el.JPG}>
+              <span className="number">{idx + 1}</span>
+            </MovieBox>
+            <MovieTitle>{el.MOVIE}</MovieTitle>
+          </div>
+        ))}
+      </Content>
     </Container>
   );
 };
@@ -30,7 +35,20 @@ const Container = styled.div`
   height: 440px;
 
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: space-around;
+`;
+
+const Title = styled.div`
+  font-size: 28px;
+  line-height: 33px;
+  border-bottom: 2px solid #d9d9d9;
+  margin-bottom: 40px;
+`;
+
+const Content = styled.div`
+  display: flex;
 `;
 
 const MovieBox = styled.div<ImgProps>`
@@ -42,6 +60,7 @@ const MovieBox = styled.div<ImgProps>`
 
   width: 180px;
   height: 320px;
+  margin: 14px;
 
   background-image: url(${props => props.img});
   background-size: 100%;
--- src/pages/MovieSearch.tsx
+++ src/pages/MovieSearch.tsx
@@ -2,19 +2,13 @@ import { styled } from 'styled-components';
 import Search from '../components/MovieSearch/Search';
 import MovieTopFive from '../components/MovieSearch/MovieTopFive';
 
-import Dummy from "../data/Dummy"
+import Dummy from '../data/Dummy';
 
 const MovieSearch = () => {
   return (
     <Container>
-      <div className='vicinity-movie'>
-        <h1>내 주변 영화관 찾기</h1>
-        <Search />
-      </div>
-      <div className='movie-top'>
-        <div className='top5'>{`TOP${Dummy.length}`}</div>
-        <MovieTopFive dummy={Dummy}/>
-      </div>
+      <Search />
+      <MovieTopFive dummy={Dummy} />
     </Container>
   );
 };
@@ -29,25 +23,4 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
   background-color: #efefef;
-
-  .vicinity-movie {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    margin: 80px 0px 100px 0px;
-  }
-
-  .movie-top {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  
-    .top5{
-      font-size: 28px;
-      line-height: 33px;
-      border-bottom: 2px solid #D9D9D9;
-      margin-bottom: 40px;
-    }
-  }
 `;

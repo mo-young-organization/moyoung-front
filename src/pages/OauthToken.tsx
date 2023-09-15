6651909b065bb4cfdd9a7ebf146c96b57916bb98
LEE2302 <dltpdudqkqh1@gmail.com>
2023-09-15 10:38:16 +0900

    로그인

--- src/pages/OauthToken.tsx
+++ src/pages/OauthToken.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
+import { styled } from 'styled-components';
 
 const OauthToken = () => {
   const navigate = useNavigate();
@@ -12,6 +13,7 @@ const OauthToken = () => {
   const nickname = searchParams.get('displayName');
   const accessTokenExpiration = searchParams.get('accessTokenExpiration');
   const refreshTokenExpiration = searchParams.get('refreshTokenExpiration');
+  console.log([user, token, refreshToken, memberId, nickname, accessTokenExpiration, refreshTokenExpiration]);
 
   useEffect(() => {
     if (user === 'false') {
@@ -19,7 +21,15 @@ const OauthToken = () => {
     }
   }, []);
 
-  return <></>;
+  return (
+    <Con>
+      <div>ㅎㅇㅎㅇㅎㅇ</div>
+    </Con>
+  );
 };
 
 export default OauthToken;
+
+const Con = styled.div`
+  background-color: red;
+`;

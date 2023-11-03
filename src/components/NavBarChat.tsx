30efb69435c9fd136e84bb0fbdf5a43ecbe5d4fe
KimHW <khw786678@gmail.com>
2023-11-03 15:58:36 +0900

    feat: 참여중인 채팅 중간저장

--- src/components/NavBarChat.tsx
+++ src/components/NavBarChat.tsx
@@ -1,18 +1,25 @@
 import { BsChatLeftDots } from 'react-icons/bs';
 import { RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri';
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 import { getChatList } from '../api/api';
 
-type PartChatList = {
+type TPartChatList = {
+  cinemaBrand: string;
+  cinemaName: string;
+  currentNum: number;
   lastMessage: null | string;
   lastMessageAt: string;
+  maxNum: number;
+  movieThumbnailUrl: string;
   recruitingArticleId: number;
+  title: string;
   unreadCount: number;
 };
 
 const NavBarChat = () => {
-  const [partChatList, setPartChatList] = useState<PartChatList[] | null>(null);
+  const [partChatList, setPartChatList] = useState<TPartChatList[] | null>(null);
   const [isListOpen, setIsListOpen] = useState<boolean>(false);
   const getChatData = async () => {
     const data = await getChatList();
@@ -26,15 +33,36 @@ const NavBarChat = () => {
 
   useEffect(() => {
     getChatData();
-  }, []);
+  }, [isListOpen]);
 
   return (
-    <div role="button" className="login text" onClick={onClickHandler}>
-      <BsChatLeftDots />
-      참여중인 채팅
-      {isListOpen ? <RiArrowUpSLine className="arrow" /> : <RiArrowDownSLine className="arrow" />}
-    </div>
+    <>
+      <div role="button" className="login text" onClick={onClickHandler}>
+        <BsChatLeftDots />
+        참여중인 채팅
+        {isListOpen ? <RiArrowUpSLine className="arrow" /> : <RiArrowDownSLine className="arrow" />}
+      </div>
+      {isListOpen && (
+        <ChatroomListWrapper>{partChatList && partChatList.map(el => <Chatroom></Chatroom>)}</ChatroomListWrapper>
+      )}
+    </>
   );
 };
+
+const ChatroomListWrapper = styled.ul`
+  position: absolute;
+  right: 150px;
+  top: 100px;
+  border-radius: 10px;
+  box-shadow: 1px 1px 3px 1px #cccccc;
+  min-width: 400px;
+  min-height: 80px;
+  background-color: #d4e3ff;
+  list-style: none;
+`;
+
+const Chatroom = styled.li`
+  padding: 16px;
+`;
 
 export default NavBarChat;

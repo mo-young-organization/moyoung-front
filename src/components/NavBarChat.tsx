48ef952964a9d8821733b7b66c190d2fe72769c2
kimhw7 <khw786678@gmail.com>
2023-11-04 17:15:30 +0900

    feat : 채팅모달 중간저장

--- src/components/NavBarChat.tsx
+++ src/components/NavBarChat.tsx
@@ -43,7 +43,26 @@ const NavBarChat = () => {
         {isListOpen ? <RiArrowUpSLine className="arrow" /> : <RiArrowDownSLine className="arrow" />}
       </div>
       {isListOpen && (
-        <ChatroomListWrapper>{partChatList && partChatList.map(el => <Chatroom></Chatroom>)}</ChatroomListWrapper>
+        <ChatroomListWrapper>
+          {partChatList &&
+            partChatList.map(el => (
+              <Chatroom>
+                <img className="thumbnail" src={el.movieThumbnailUrl} alt="영화 썸네일 이미지" />
+                <ChatRoomInfoWrapper>
+                  <div className="title">{el.title}</div>
+                  <div className="cinemaInfo">
+                    <span>{el.cinemaBrand}</span>
+                    <span className="cinemaName">{el.cinemaName}</span>
+                  </div>
+                </ChatRoomInfoWrapper>
+                <MemberCountWrapper>
+                  <span className="memberCount">
+                    {el.currentNum} / {el.maxNum}
+                  </span>
+                </MemberCountWrapper>
+              </Chatroom>
+            ))}
+        </ChatroomListWrapper>
       )}
     </>
   );
@@ -63,6 +82,46 @@ const ChatroomListWrapper = styled.ul`
 
 const Chatroom = styled.li`
   padding: 16px;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #d4e3ff;
+  border-radius: 10px;
+  &&:hover {
+    background-color: #9dbffe;
+    cursor: pointer;
+  }
+  .thumbnail {
+    max-height: 100%;
+    border-radius: 5px;
+  }
+`;
+const ChatRoomInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: space-between;
+  .title {
+    font-weight: bold;
+    font-size: 16px;
+  }
+  .cinemaInfo {
+    .cinemaName {
+      font-size: 14px;
+      color: #686868;
+    }
+  }
+`;
+
+const MemberCountWrapper = styled.div`
+  display: flex;
+  align-items: start;
+  height: 100%;
+  .memberCount {
+    color: #5b5b5b;
+    font-size: 14px;
+  }
 `;
 
 export default NavBarChat;

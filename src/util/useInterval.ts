27136c28f2d135a05242383b29fd2e809e0a02dc
LEE2302 <dltpdudqkqh1@gmail.com>
2023-11-27 22:17:50 +0900

    ref 수정

--- src/util/useInterval.ts
+++ src/util/useInterval.ts
@@ -1,8 +1,8 @@
-import { useRef, useEffect, useCallback } from 'react';
+import { useRef, useEffect } from 'react';
 
 function useInterval(callback, delay) {
-  const savedCallback = useRef();
-  const intervalIdRef = useRef();
+  const savedCallback = useRef(null);
+  const intervalIdRef = useRef(null);
 
   useEffect(() => {
     savedCallback.current = callback;
